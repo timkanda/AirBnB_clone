cf6af9021f543458004234fccb49055c2b693857
timkanda <tmthkanda@gmail.com>
2023-02-11 04:35:54 -0800

    pushing branch

--- models/base_model.py
+++ models/base_model.py
@@ -1,52 +1,49 @@
 #!/usr/bin/python3
 """defining Basemodel."""
 
-import models
+from models import storage
 from uuid import uuid4
-from datetime import datetime
+from datetime import datetime, time, date
 
 
-class Basemodel:
-    """"representing a basemodel.
-
-    Attributes:
-        id (string): unique id which must be converted to string.
-        created_at (datetime): when the instance is created.
-        updated_at (datetime): will be updated everytime the object changes.
+class BaseModel:
+    """a class that defines attributes id,
+    created_at, updated_at and methods
     """
-    id = str(uuid4())
-    created_at = datetime.now()
-    updated_at = datetime.now()
 
-    def __init__(self, *args, **kwags):
-        """building dictonary."""
-        if (len(kwargs) != 0):
-            for key, value in kwargs.items():
-                if (key == 'id'):
-                    self.id = kwargs.get(key)
-                if (key == 'created_at'):
-                    self.created_at = datetime.strptime(
-                        kwargs.get(key), '%Y-%m-%dT%H:%M:%S.%f')
-                if (key == 'updated_at'):
-                    self.updated_at = datetime.strptime(
-                        kwargs.get(key), '%Y-%m-%dT%H:%M:%S.%f')
-        else:
-            self.id = str(uuid())
+    def __init__(self, *args, **kwargs):
+        """constructor for class attrs id
+        created_at and updated_at
+        """
+        if not kwargs:
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
+        else:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key in ["created_at", "updated_at"]:
+                        value = datetime.\
+                            strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
 
     def __str__(self):
-        """prints string method."""
-        return ("[{self.__class__.__name__}] ({}) {}"
-                .format(self.id, self.__dict__, self=self))
+        """returns a string repr of the class
+         """
+        return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
 
     def save(self):
+        """updates the updated_at attr."""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
-        """returns a dictionary."""
+        """returns a dictionary repr of the instance."""
         my_dict = self.__dict__.copy()
-        my_dict['created_at'] = self.created_at.isoformat()
-        my_dict['updated_at'] = self.updated_at.isoformat()
-        my_dict['__class__'] = self.__class__.__name__
-        return (my_dict)
+        my_dict.update({
+            "__class__": self.__class__.__name__,
+            "updated_at": self.updated_at.isoformat(),
+            "created_at": self.created_at.isoformat()
+            })
+        return my_dict
